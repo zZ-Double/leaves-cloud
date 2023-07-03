a48d4583bfd5e454f367e1ee62979b7d1b552dec
leaves <942400970@qq.com>
2023-07-03 23:26:45 +0800

    基础类新增

---
+++ leaves-common/common-core/src/main/java/com/leaves/common/base/IBaseEnum.java
@@ -0,0 +1,83 @@
+package com.leaves.common.base;
+
+
+import cn.hutool.core.util.ObjectUtil;
+
+import java.util.EnumSet;
+import java.util.Objects;
+
+
+public interface IBaseEnum<T> {
+
+    T getValue();
+
+    String getName();
+
+    /**
+     * 根据值获取枚举
+     *
+     * @param value
+     * @param clazz
+     * @param <E>   枚举
+     * @return
+     */
+    static <E extends Enum<E> & IBaseEnum> E getEnumByValue(Object value, Class<E> clazz) {
+        Objects.requireNonNull(value);
+        EnumSet<E> allEnums = EnumSet.allOf(clazz); // 获取类型下的所有枚举
+        E matchEnum = allEnums.stream()
+                .filter(e -> ObjectUtil.equal(e.getValue(), value))
+                .findFirst()
+                .orElse(null);
+        return matchEnum;
+    }
+
+    /**
+     * 根据文本标签获取值
+     *
+     * @param value
+     * @param clazz
+     * @param <E>
+     * @return
+     */
+    static <E extends Enum<E> & IBaseEnum> String getLabelByValue(Object value, Class<E> clazz) {
+        Objects.requireNonNull(value);
+        EnumSet<E> allEnums = EnumSet.allOf(clazz); // 获取类型下的所有枚举
+        E matchEnum = allEnums.stream()
+                .filter(e -> ObjectUtil.equal(e.getValue(), value))
+                .findFirst()
+                .orElse(null);
+
+        String label = null;
+        if (matchEnum != null) {
+            label = matchEnum.getName();
+        }
+        return label;
+    }
+
+
+    /**
+     * 根据文本标签获取值
+     *
+     * @param label
+     * @param clazz
+     * @param <E>
+     * @return
+     */
+    static <E extends Enum<E> & IBaseEnum> Object getValueByLabel(String label, Class<E> clazz) {
+        Objects.requireNonNull(label);
+        EnumSet<E> allEnums = EnumSet.allOf(clazz); // 获取类型下的所有枚举
+        String finalLabel = label;
+        E matchEnum = allEnums.stream()
+                .filter(e -> ObjectUtil.equal(e.getName(), finalLabel))
+                .findFirst()
+                .orElse(null);
+
+        Object value = null;
+        if (matchEnum != null) {
+            value = matchEnum.getValue();
+        }
+        return value;
+    }
+
+
+}
---
+++ leaves-common/common-core/src/main/java/com/leaves/common/enums/StatusEnum.java
@@ -0,0 +1,22 @@
+package com.leaves.common.enums;
+
+import com.leaves.common.base.IBaseEnum;
+import lombok.Getter;
+
+
+public enum StatusEnum implements IBaseEnum<Integer> {
+
+    DISABLE (0, "禁用"), // false
+    ENABLE(1, "启用"); // true
+
+    @Getter
+    private Integer value;
+
+    @Getter
+    private String name;
+
+    StatusEnum(Integer value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+}
