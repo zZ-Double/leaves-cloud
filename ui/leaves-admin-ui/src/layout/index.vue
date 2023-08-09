d5fe2ec94919f852058910f8c7fae9ae4b35def0
leaves <942400970@qq.com>
2023-08-09 17:30:46 +0800

    fix: 修改layou组件

--- ui/leaves-admin-ui/src/layout/index.vue
+++ ui/leaves-admin-ui/src/layout/index.vue
@@ -1,22 +1,61 @@
 <template>
-    <div class="app-wrapper">
-        <div class="drawer-bg"/>
-        <Sidebar class="sidebar-container" />
-        <!-- <div class="main-container">
-            <div>
-                <navbar />
-                <tags-view />
-            </div>
-            <app-main />
-            <RightPanel>
-                <settings />
-            </RightPanel>
-        </div> -->
+    <div :class="classObj" class="app-wrapper">
+      <div
+        v-if="device === 'mobile' && sidebar.opened"
+        class="drawer-bg"
+        @click="handleClickOutside"
+      />
+      <Sidebar class="sidebar-container" />
+      <!-- <div :class="{ hasTagsView: needTagsView }" class="main-container">
+        <div :class="{ 'fixed-header': fixedHeader }">
+          <navbar />
+          <tags-view v-if="needTagsView" />
+        </div>
+        <app-main />
+        <RightPanel v-if="showSettings">
+          <settings />
+        </RightPanel>
+      </div> -->
     </div>
-</template>
+  </template>
   
 <script setup lang="ts">
 import Sidebar from './components/Sidebar/index.vue'
+import { computed, watchEffect } from 'vue';
+import { useWindowSize } from '@vueuse/core';
+
+import useStore from '@/store';
+
+const { width } = useWindowSize();
+const WIDTH = 992;
+
+const { app } = useStore();
+
+const sidebar = computed(() => app.sidebar);
+const device = computed(() => app.device);
+// const needTagsView = computed(() => setting.tagsView);
+// const fixedHeader = computed(() => setting.fixedHeader);
+// const showSettings = computed(() => setting.showSettings);
+
+const classObj = computed(() => ({
+  hideSidebar: !sidebar.value.opened,
+  openSidebar: sidebar.value.opened,
+  withoutAnimation: sidebar.value.withoutAnimation,
+  mobile: device.value === 'mobile',
+}));
+
+watchEffect(() => {
+  if (width.value < WIDTH) {
+    app.toggleDevice('mobile');
+    app.closeSideBar(true);
+  } else {
+    app.toggleDevice('desktop');
+  }
+});
+
+function handleClickOutside() {
+  app.closeSideBar(false);
+}
 
 </script>
   
