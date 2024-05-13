0547740291be95a66fc399d549414c345b535be2
leaves <942400970@qq.com>
2024-05-13 09:09:07 +0800

    fix: 代码格式调整

--- leaves-auth/src/main/java/com/leaves/auth/enums/PasswordEncoderTypeEnum.java
+++ leaves-auth/src/main/java/com/leaves/auth/enums/PasswordEncoderTypeEnum.java
@@ -8,14 +8,14 @@ import lombok.Getter;
 
 public enum PasswordEncoderTypeEnum {
 
-    BCRYPT("{bcrypt}","BCRYPT加密"),
-    NOOP("{noop}","无加密明文");
+    BCRYPT("{bcrypt}", "BCRYPT加密"),
+    NOOP("{noop}", "无加密明文");
 
     @Getter
     private String prefix;
 
-    PasswordEncoderTypeEnum(String prefix, String desc){
-        this.prefix=prefix;
+    PasswordEncoderTypeEnum(String prefix, String desc) {
+        this.prefix = prefix;
     }
 
 }
